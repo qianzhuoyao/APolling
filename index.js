fbe3b06e6d416eb9e3dc77df23361a37c7c631fa
QaeJey <1059116864@qq.com>
2022-11-12 23:28:22 +0800

    feat:加入心跳机制，在检测到>=10s内无继续触发的事件流，则重置轮询并再次触发Running.取消定时概念为不设置定时则为一直持续

--- index.js
+++ index.js
@@ -1,165 +1,191 @@
-import {catchError, from, of, throwError, timer} from 'rxjs'
-import {map, exhaustMap, delay, tap, takeWhile} from 'rxjs/operators'
+import { catchError, from, of, throwError, timer } from "rxjs";
+import { map, exhaustMap, delay, tap, takeWhile } from "rxjs/operators";
+import { ACollector } from "./collector";
 
-const BASE_TYPE = ['number', 'string', 'boolean', 'function']
-const DEFAULT_WAIT_TIME = 2
+const BASE_TYPE = ["number", "string", "boolean", "function"];
+const DEFAULT_WAIT_TIME = 2;
 export default class Polling {
-    constructor(waitTime, ageing) {
-        this.timer = null
-        this.delayTime = 0
-        this.stop = false
-        this.preValueOfPollingEffect = null
-        this.ageing = parseFloat(ageing) * 1000 || 120 * 1000
-        this.setWaitTime(waitTime)
-        this.countdown()
-    }
+  constructor(waitTime, ageing) {
+    this.timer = null;
+    this.delayTime = 0;
+    this.stop = false;
+    this.preValueOfPollingEffect = null;
+    this.ageing = outPutNumber(ageing) * 1000;
+    this.setWaitTime(waitTime);
+    this.countdown();
+  }
 
-    countdown() {
-        setTimeout(() => {
-            this.over()
-        }, this.ageing)
-    }
+  outPutNumber(anything) {
+    return Number(anything) || 0;
+  }
 
-    check(anything, checkType) {
-        if (BASE_TYPE.includes(checkType)) {
-            return typeof anything === checkType
-        } else {
-            throw new Error('类型不被允许')
-        }
+  countdown() {
+    if (!!this.ageing) {
+      setTimeout(() => {
+        this.over();
+      }, this.ageing);
     }
+  }
 
-    setWaitTime(waitTime) {
-        this.waitTime = this.isNumber(waitTime) ? waitTime : DEFAULT_WAIT_TIME
+  check(anything, checkType) {
+    if (BASE_TYPE.includes(checkType)) {
+      return typeof anything === checkType;
+    } else {
+      throw new Error("类型不被允许");
     }
+  }
 
-    isNumber(anything) {
-        return this.check(anything, 'number')
+  setWaitTime(waitTime) {
+    this.waitTime = this.isNumber(waitTime) ? waitTime : DEFAULT_WAIT_TIME;
+  }
+
+  isNumber(anything) {
+    return this.check(anything, "number");
+  }
+
+  isFunc(anything) {
+    return this.check(anything, "function");
+  }
+
+  pollingCondition(stopCondition) {
+    if (this.isFunc(stopCondition)) {
+      this.pollingContinue = stopCondition;
     }
+    return this;
+  }
 
-    isTruth(a) {
-        return !!a
+  effectHook(effect) {
+    if (this.isFunc(effect)) {
+      this.effect = effect;
     }
+    return this;
+  }
 
-    isFunc(anything) {
-        return this.check(anything, 'function')
+  over() {
+    if (this.timer) {
+      this.timer.unsubscribe();
+      this.timer = null;
     }
+  }
 
-    pollingCondition(stopCondition) {
-        if (this.isFunc(stopCondition)) {
-            this.pollingContinue = stopCondition
-        }
-        return this
+  finishedHook(callback) {
+    if (this.isFunc(callback)) {
+      this.finished = callback;
     }
+    return this;
+  }
 
-    effectHook(effect) {
-        if (this.isFunc(effect)) {
-            this.effect = effect
-        }
-        return this
+  forceChangeResponse(valueBuildCallback) {
+    if (this.isFunc(valueBuildCallback)) {
+      this.valueBuildFn = valueBuildCallback;
     }
+    return this;
+  }
 
-    over() {
-        this.timer && this.timer.unsubscribe()
+  pauseRequest(fn) {
+    if (this.isFunc(fn)) {
+      this.runPause = fn;
     }
-
-    finishedHook(callback) {
-        if (this.isFunc(callback)) {
-            this.finished = callback
-        }
-        return this
+    return this;
+  }
+  catchReject(fn) {
+    if (this.isFunc(fn)) {
+      this.reject = fn;
     }
+    return this;
+  }
 
-    forceChangeResponse(valueBuildCallback) {
-        if (this.isFunc(valueBuildCallback)) {
-            this.valueBuildFn = valueBuildCallback
-        }
-        return this
-    }
-
-    pauseRequest(fn) {
-        if (this.isFunc(fn)) {
-            this.runPause = fn
-        }
-        return this
-    }
-
-    catchReject(fn) {
-        if (this.isFunc(fn)) {
-            this.reject = fn
-        }
-        return this
-    }
-
-    delay(time) {
-        this.delayTime = this.isNumber(time) ? time : 0
-        return this
-    }
-
-    running(pollingRequest) {
-        const that = this
-        if (this.isFunc(pollingRequest)) {
-            this.timer = timer(0, this.waitTime * 1000)
-                .pipe(
-                    exhaustMap(() => {
-                        if (this.isFunc(this.runPause) && this.runPause(this.preValueOfPollingEffect)) {
-                            return of(null)
-                        } else {
-                            return from(pollingRequest()).pipe(
-                                delay(this.delayTime),
-                                catchError(err => {
-                                    if (this.reject) {
-                                        this.reject(err)
-                                        this.stop = true
-                                        return of(err)
-                                    } else {
-                                        return throwError(`轮询异常,接口被拒绝,running回调函数${pollingRequest.name}可能不是一个可以被有效使用的接口`)
-                                    }
-                                })
-                            )
-                        }
-                    }),
-                    map(r => {
-                        try {
-                            let otherValue = {}
-                            if (this.isFunc(this.valueBuildFn) && !this.stop) {
-                                //非空处理
-                                otherValue = this.valueBuildFn(r) || {}
-                            }
-                            return {
-                                ...r,
-                                ...otherValue,
-                            }
-                        } catch (e) {
-                            this.reject && this.reject(e)
-                            this.stop = true
-                        }
-                    }),
-                    tap((pollingCallbackResult) => {
-                        try {
-                            this.isFunc(this.effect) && !this.stop && this.effect(pollingCallbackResult, this.preValueOfPollingEffect)
-                            this.preValueOfPollingEffect = pollingCallbackResult
-                        } catch (e) {
-                            this.reject && this.reject(e)
-                            this.stop = true
-                        }
-                    }),
-                    takeWhile(pollingCallbackResult => {
-                        try {
-                            return this.isFunc(this.pollingContinue) && this.pollingContinue(pollingCallbackResult) && !this.stop
-                        } catch (e) {
-                            this.reject && this.reject(e)
-                            return false
-                        }
-                    })
-                )
-                .subscribe({
-                    error(err) {
-                        throw new Error(`轮询操作失败 信息如下:${err}`)
-                    },
-                    complete() {
-                        that.isFunc(that.finished) && that.finished()
-                    }
+  delay(time) {
+    this.delayTime = this.isNumber(time) ? time : 0;
+    return this;
+  }
+  running(pollingRequest) {
+    const that = this;
+    if (this.isFunc(pollingRequest)) {
+      this.timer = timer(0, this.waitTime * 1000)
+        .pipe(
+          exhaustMap(() => {
+            if (
+              this.isFunc(this.runPause) &&
+              this.runPause(this.preValueOfPollingEffect)
+            ) {
+              return of(null);
+            } else {
+              return from(pollingRequest()).pipe(
+                delay(this.delayTime),
+                catchError((err) => {
+                  if (this.reject) {
+                    this.reject(err);
+                    this.stop = true;
+                    return of(err);
+                  } else {
+                    return throwError(
+                      `轮询异常,接口被拒绝,running回调函数${pollingRequest.name}可能不是一个可以被有效使用的接口`
+                    );
+                  }
                 })
-        }
+              );
+            }
+          }),
+          map((r) => {
+            try {
+              let otherValue = {};
+              if (this.isFunc(this.valueBuildFn) && !this.stop) {
+                //非空处理
+                otherValue = this.valueBuildFn(r) || {};
+              }
+              return {
+                ...r,
+                ...otherValue,
+              };
+            } catch (e) {
+              this.reject && this.reject(e);
+              this.stop = true;
+            }
+          }),
+          tap((pollingCallbackResult) => {
+            try {
+              //心跳检测,当等待时间+1后未获取继续的任务，则触发心跳机制重新跑轮询
+              new ACollector(this.waitTime + 10000).do(() => {
+                return new Promise(() => {
+                  this.over();
+                  this.running(pollingRequest);
+                  new ACollector.reset();
+                });
+              });
+              this.isFunc(this.effect) &&
+                !this.stop &&
+                this.effect(
+                  pollingCallbackResult,
+                  this.preValueOfPollingEffect
+                );
+              this.preValueOfPollingEffect = pollingCallbackResult;
+            } catch (e) {
+              this.reject && this.reject(e);
+              this.stop = true;
+            }
+          }),
+          takeWhile((pollingCallbackResult) => {
+            try {
+              return (
+                this.isFunc(this.pollingContinue) &&
+                this.pollingContinue(pollingCallbackResult) &&
+                !this.stop
+              );
+            } catch (e) {
+              this.reject && this.reject(e);
+              return false;
+            }
+          })
+        )
+        .subscribe({
+          error(err) {
+            throw new Error(`轮询操作失败 信息如下:${err}`);
+          },
+          complete() {
+            that.isFunc(that.finished) && that.finished();
+          },
+        });
     }
+  }
 }
